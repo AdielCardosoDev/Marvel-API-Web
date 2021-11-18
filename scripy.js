87cec2f383c9d524be1e80a71799bd02f180a7a2
Adiel <adieldeveloper@gmail.com>
2021-11-18 18:50:31 -0300

    MarvelApiWeb

---
+++ scripy.js
@@ -0,0 +1,19 @@
+const ts = "1637196537";
+const apiKey = "7b5215aefe2005ecd1258ad1db3e0b6e";
+const md5 = "41a6fd74573aa071f256320911efae84";
+
+const url = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${apiKey}&hash=${md5}&limit=6`;
+
+function getMarvel() {
+  axios.get(url)
+    .then((response) => {
+      const data = response.data
+      console.log(data)
+    })
+    .catch((error) => console.log(error));
+}
+
+getMarvel();
+
+
+
